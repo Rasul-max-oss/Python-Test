29d36ddc953e10e9efae1df77777d70e844dc932
Xelleet <ererenyeager@yandex.ru>
2026-03-20 19:32:01 +0300

    update

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
 def main():
     print("=== КАЛЬКУЛЯТОР ===")
     x = float(input("Введите первое число: "))
@@ -11,6 +17,8 @@ def main():
     
     print(f"{x} + {y} = {add(x, y)}")
     print(f"{x} - {y} = {subtract(x, y)}")
+    print(f'{x} * {y} = {multiply(x, y)}')
+    print(f'{x} / {y} = {divide(x, y)}')
 
 if __name__ == "__main__":
     main()
