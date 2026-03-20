f7ad15c38e5c18063e9da57cb5b352f946e280d8
Rasul <rasul.rzaev.2014@mail.ru>
2026-03-21 00:30:41 +0800

    create calculator app

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-print("Hello world")
-print("This is a test of the main.py file.")
-print('Some text')
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def main():
+    print("=== КАЛЬКУЛЯТОР ===")
+    x = float(input("Введите первое число: "))
+    y = float(input("Введите второе число: "))
+    
+    print(f"{x} + {y} = {add(x, y)}")
+    print(f"{x} - {y} = {subtract(x, y)}")
+
+if __name__ == "__main__":
+    main()
